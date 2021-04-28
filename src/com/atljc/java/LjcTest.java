131d00e8176c58d793d01f75fa7478f06c10658f
liujc 刘杰超 <liujc@nysoftland.com.cn>
2021-04-28 16:52:25 +0800

    Initial commit

---
+++ src/com/atljc/java/LjcTest.java
@@ -0,0 +1,14 @@
+package com.atljc.java;
+
+import java.util.ArrayList;
+
+public class LjcTest {
+    public static void main(String[] args) {
+        System.out.println("Ljc");
+        ArrayList list = new ArrayList();
+    }
+
+    public void method(){
+        System.out.println("");
+    }
+}
